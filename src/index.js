8cbe80e5fa8d2ce483b94ec18172b960e7ac50b3
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-05-29 20:11:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import fetchImages from "./js/fetch";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import "../src/sass/main.scss";
-import cardTemplate from  "./js/templade.hbs"
-// import axios from "axios";
+import cardTemplate from "./js/templade.hbs";
 
 const { searchForm, gallery, loadMoreBtn, endCollectionText } = {
   searchForm: document.querySelector('.search-form'),
@@ -20,28 +19,6 @@ function renderCardImage(arr) {
     const markup = arr.map(item => cardTemplate(item)).join('');
     gallery.insertAdjacentHTML('beforeend', markup)
 }
-
-// Шаблон
-// function cardTemplate() {
-//     return `
-//     <div class="photo-card">
-//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>${likes}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>${views}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>${comments}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>${downloads}</b>
-//     </p>
-//   </div>
-// </div>`
-// }
 
 let lightbox = new SimpleLightbox('.photo-card a', {
   captions: true,
